4dc7870948bb42032853552019d55dbfa5423ed7
Franklin "Snaipe" Mathieu <snaipe@diacritic.io>
2018-01-29 14:37:35 +0000

    zsh: retabbed prompt file

--- .config/zsh/init/50-prompt.zsh
+++ .config/zsh/init/50-prompt.zsh
@@ -7,21 +7,21 @@ CURRENT_BG='NONE'
 SEGMENT_SEPARATOR=''
 
 case "$SESSION_TYPE" in
-      remote/*) primary_color=green;;
-             *) primary_color=magenta;;
+	  remote/*) primary_color=green;;
+	         *) primary_color=magenta;;
 esac
 
 prompt_segment() {
-  local bg fg
-  [[ -n $1 ]] && bg="%K{$1}" || bg="%k"
-  [[ -n $2 ]] && fg="%F{$2}" || fg="%f"
-  if [[ $CURRENT_BG != 'NONE' && $1 != $CURRENT_BG ]]; then
-    echo -n " %{$bg%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR%{$fg%} "
-  else
-    echo -n "%{$bg%}%{$fg%} "
-  fi
-  CURRENT_BG=$1
-  [[ -n $3 ]] && print $4 -n $3
+	local bg fg
+	[[ -n $1 ]] && bg="%K{$1}" || bg="%k"
+	[[ -n $2 ]] && fg="%F{$2}" || fg="%f"
+	if [[ $CURRENT_BG != 'NONE' && $1 != $CURRENT_BG ]]; then
+		echo -n " %{$bg%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR%{$fg%} "
+	else
+		echo -n "%{$bg%}%{$fg%} "
+	fi
+	CURRENT_BG=$1
+	[[ -n $3 ]] && print $4 -n $3
 }
 
 prompt_context() {
@@ -37,13 +37,13 @@ prompt_dir() {
 }
 
 prompt_end() {
-  if [[ -n $CURRENT_BG ]]; then
-    echo -n " %{%k%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR"
-  else
-    echo -n "%{%k%}"
-  fi
-  echo -n "%{%f%}"
-  CURRENT_BG=''
+	if [[ -n $CURRENT_BG ]]; then
+		echo -n " %{%k%F{$CURRENT_BG}%}$SEGMENT_SEPARATOR"
+	else
+		echo -n "%{%k%}"
+	fi
+	echo -n "%{%f%}"
+	CURRENT_BG=''
 }
 
 build_prompt() {
