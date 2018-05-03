7ba41ce9fc7c2e3fbbfe55022cccabf9580df90d
Franklin "Snaipe" Mathieu <snaipe@diacritic.io>
2018-05-03 05:13:17 -0700

    zsh: add smart sourcing for convenient dotfile sourcing

--- .config/zsh/init/0-dot.zsh
+++ .config/zsh/init/0-dot.zsh
@@ -1,1 +1,16 @@
 alias dot='PATH="$PATH:$HOME/.config/dotgit/bin" git --git-dir="$HOME/.config/dotgit/repo" --work-tree="$HOME"'
+
+source() {
+    what="$1"
+    case "$what" in
+        ./*|../*|~*|/*)
+            ;;
+        *)
+            what="$(dot i-filept "$@" | head -n 1)";;
+    esac
+    builtin source "$what"
+}
+
+.() {
+    source "$@"
+}
